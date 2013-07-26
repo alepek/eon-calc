0e7dbebdacfc53734f695156c638d56e25ca6078
Alexander Pekkari <pekkarih@gmail.com>
2013-07-26 21:15:07 +0200

    Dice rolls, hitareas and more.

--- js/main.js
+++ js/main.js
@@ -32,6 +32,11 @@ $(function()
 	GC.AllPanes = jQuery(".pagePane")
 	GC.CalculatorPane = jQuery(".calculatorPane");
 	GC.HomePane = jQuery(".homePane");
+	GC.DamageInput = jQuery("#damageInput");
+
+	GC.crushButton = jQuery(".damageButton.crush");
+	GC.pokeButton = jQuery(".damageButton.poke");
+	GC.chopButton = jQuery(".damageButton.chop");
 
 	GC.HomeButton.click(function()
 	{
@@ -44,6 +49,18 @@ $(function()
 	GC.HelpButton.click(function()
 	{
 		switchPanes(jQuery(this));
+	});
+	GC.crushButton.click(function()
+	{
+		ResultUpdater.GenerateResultOutput();
+	});
+	GC.pokeButton.click(function()
+	{
+		ResultUpdater.GenerateResultOutput();
+	});
+	GC.chopButton.click(function()
+	{
+		ResultUpdater.GenerateResultOutput();
 	});
 });
 
@@ -83,3 +100,266 @@ switchPanes = function(targetPaneButton)
 	}
 }
 
+ResultUpdater = 
+{
+	CheckDamageValue: function(string)
+	{
+		var isnum = /^\d+$/.test(string);
+		if(!isnum)
+		{
+			// visa fel
+			return false;
+		}
+		return true;
+	},
+	GenerateResultOutput: function()
+	{
+		var damageValue = GC.DamageInput.val();
+		if(!ResultUpdater.CheckDamageValue(damageValue))
+		{
+			// show warning
+			return;
+		}
+
+		damageValue = parseInt(damageValue);
+		if(damageValue > 100)
+		{
+			// show invalid input warning - only gods hit this hard, and we are mere mortals.
+			// also I am too lazy to design the gui properly for such huge amounts of extra damage.
+			return;
+		}
+
+		// alright, let's get cracking.
+	}
+
+}
+
+DiceRoller = 
+{
+	RollD4: function(numDice, exploding)
+	{
+		return DiceRoller.GDR(4, numDice, exploding);
+	},
+	RollD6: function(numDice, exploding)
+	{
+		return DiceRoller.GDR(6, numDice, exploding);
+	},
+	RollD8: function(numDice, exploding)
+	{
+		return DiceRoller.GDR(8, numDice, exploding);
+	},
+	RollD10: function(numDice, exploding)
+	{
+		return DiceRoller.GDR(10, numDice, exploding);
+	},
+	RollD12: function(numDice, exploding)
+	{
+		return DiceRoller.GDR(12, numDice, exploding);
+	},
+	RollD20: function(numDice, exploding)
+	{
+		return DiceRoller.GDR(20, numDice, exploding);
+	},
+	RollD100: function(numDice, exploding)
+	{
+		return DiceRoller.GDR(100, numDice, exploding);
+	},
+	GDR: function(diceMax, numDice, exploding)
+	{
+		var rollDie = function(max, resultobject, exploding)
+		{
+			var rand = Math.ceil(Math.random()*max);
+			resultobject.totalRolled += 1;
+			resultobject.rolls.push(rand);
+			if(rand === max && exploding)
+			{ // explosion!
+				resultobject.explosions += 1;
+				return rand+rollDie(max, resultobject, exploding); 
+				// Surely this recursive behavior will never betray me.
+			}
+			else
+			{
+				return rand;
+			}
+		}
+
+		// this object tracks all the rolls and stuff.
+		var resObj = {};
+		resObj.totalRolled = 0;
+		resObj.explosions = 0;
+		resObj.result = 0;
+		resObj.rolls = [];
+
+		for(var i=0;i<numDice;i++)
+		{
+			resObj.result += rollDie(diceMax, resObj, exploding);
+		}
+
+		return resObj;
+	}
+};
+
+EonRules =
+{	
+	GetHitAreaFromRoll: function(diceRollResult)
+	{
+		// this is gonny be stringly typed, due to laziness
+		var H = HitAreas;
+		var r = diceRollResult.result;
+		if(r < 21)
+			return H.head;
+		if(r<41)
+			return H.leftArm;
+		if(r<61)
+			return H.rightArm;
+		if(r<71)
+			return H.chest;
+		if(r<81)
+			return H.guts;
+		if(r<91)
+			return H.leftLeg;
+
+		return H.rightLeg;
+	},
+	GetSubHitAreaFromRoll: function(diceRollResult, majorHitArea)
+	{
+		var H = HitAreas;
+		var r = diceRollResult.result;
+
+		if(majorHitArea.title === H.head.title)
+		{
+			if(r < 5)
+				return H.head.face;
+			if(r<9)
+				return H.head.skull;
+			return H.head.throat;
+		}
+		if(majorHitArea.title === H.leftArm.title)
+		{
+			if(r < 3)
+				return H.leftArm.shoulder;
+			if(r < 5)
+				return H.leftArm.upperarm;
+			if(r < 6)
+				return H.leftArm.elbow;
+			if(r < 9)
+				return H.leftArm.forearm;
+			return H.leftArm.hand;
+		}
+		if(majorHitArea.title === H.rightArm.title)
+		{
+			if(r < 3)
+				return H.rightArm.shoulder;
+			if(r < 5)
+				return H.rightArm.upperarm;
+			if(r < 6)
+				return H.rightArm.elbow;
+			if(r < 9)
+				return H.rightArm.forearm;
+			return H.rightArm.hand;
+		}
+		if(majorHitArea.title === H.chest.title)
+		{
+			return H.chest.chest;
+		}
+		if(majorHitArea.title === H.guts.title)
+		{
+			if(r < 9)
+				return H.guts.guts;
+			return H.guts.privates;
+		}
+		if(majorHitArea.title === H.leftLeg.title)
+		{
+			if(r < 3)
+				return H.leftLeg.hip;
+			if(r < 5)
+				return H.leftLeg.thigh;
+			if(r < 7)
+				return H.leftLeg.knee;
+			if(r < 10)
+				return H.leftLeg.calf;
+			return H.leftLeg.foot;
+		}
+		if(majorHitArea.title === H.rightLeg.title)
+		{
+			if(r < 3)
+				return H.rightLeg.hip;
+			if(r < 5)
+				return H.rightLeg.thigh;
+			if(r < 7)
+				return H.rightLeg.knee;
+			if(r < 10)
+				return H.rightLeg.calf;
+			return H.rightLeg.foot;
+		}
+	},
+	RollForHit: function()
+	{
+		var d100 = DiceRoller.RollD100(1, false);
+		var d10 = DiceRoller.RollD10(1, false);
+
+		var hitArea = EonRules.GetHitAreaFromRoll(d100);
+		var subHitArea = EonRules.GetSubHitAreaFromRoll(d10, hitArea);
+		subHitArea.extraDamages = [];
+		for(var i=0;i<5;i++)
+		{
+			subHitArea.extraDamages.push(DiceRoller.RollD10(1,false).result);
+		}
+
+		return {hitArea:hitArea, subHitArea:subHitArea};
+	}
+};
+
+HitAreas = {};
+
+HitAreas.head= {};
+HitAreas.head.title = "huvud";
+HitAreas.head.face = {};
+HitAreas.head.skull = {};
+HitAreas.head.throat = {};
+HitAreas.head.face.title = "ansikte"
+HitAreas.head.skull.title = "skalle";
+HitAreas.head.throat.title =  "hals";
+
+HitAreas.leftArm= {};
+HitAreas.leftArm.title= "vänster arm";
+HitAreas.leftArm.shoulder = "vänster skuldra";
+HitAreas.leftArm.upperarm=  "vänster överarm";
+HitAreas.leftArm.elbow = "vänster armbåge";
+HitAreas.leftArm.forearm = "vänster underarm";
+HitAreas.leftArm.hand = "vänster hand";
+
+HitAreas.rightArm= {};
+HitAreas.rightArm.title = "höger arm";
+HitAreas.rightArm.shoulder = "höger skuldra";
+HitAreas.rightArm.upperarm=  "höger överarm";
+HitAreas.rightArm.elbow = "höger armbåge";
+HitAreas.rightArm.forearm = "höger underarm";
+HitAreas.rightArm.hand = "höger hand";
+
+HitAreas.chest= {};
+HitAreas.chest.title = "bröstkorg";
+HitAreas.chest.chest=  "bröstkorg";
+
+HitAreas.guts= {};
+HitAreas.guts.title = "mage";
+HitAreas.guts.guts=  {};
+HitAreas.guts.guts.title=  "mage";
+HitAreas.guts.privates=  {};
+HitAreas.guts.privates.title=  "underliv";
+
+HitAreas.leftLeg= {};
+HitAreas.leftLeg.title = "vänster ben";
+HitAreas.leftLeg.hip = "vänster höft";
+HitAreas.leftLeg.thigh = "vänster lår";
+HitAreas.leftLeg.knee = "vänster knä";
+HitAreas.leftLeg.calf = "vänster vad";
+HitAreas.leftLeg.foot = "vänster fot";
+
+HitAreas.rightLeg= {};
+HitAreas.rightLeg.title = "höger ben";
+HitAreas.rightLeg.hip = "höger höft";
+HitAreas.rightLeg.thigh = "höger lår";
+HitAreas.rightLeg.knee = "höger knä";
+HitAreas.rightLeg.calf = "höger vad";
+HitAreas.rightLeg.foot = "höger fot";
